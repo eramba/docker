1f3bc4e57bce622bb3c9cbbbf3ed8fd46acf5087
Martin <martin@eramba.org>
2022-10-26 12:10:50 +0200

    Added a CRON container entrypoint
    That stores container environmental values needed for running eramba to the actual .env file, othewise cron job has problems picking up environmentals that are ran with the container from docker compose

---
+++ docker-cron-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /var/www/eramba/app/upgrade/vendor/eramba/docker || exit
+
+su -s /bin/bash -c "printenv | grep DB_ > .env" www-data
+su -s /bin/bash -c "printenv | grep CACHE_URL >> .env" www-data
+su -s /bin/bash -c "printenv | grep HTTP_HOST >> .env" www-data
+
+su -s /bin/bash -c "crontab -u www-data /etc/cron.d/eramba-crontab" www-data
+
+exec docker-php-entrypoint "$@"
