ec458c12904f5a2bb0187d2b7582440445d63203
Martin <martin@eramba.org>
2022-10-31 14:35:07 +0100

    Added a `PUBLIC_ADDRESS` env() value
    which will get compatibility with eramba images soon to prevent HTTP_HOST security header issue

--- docker-cron-entrypoint.sh
+++ docker-cron-entrypoint.sh
@@ -4,6 +4,7 @@ cd /var/www/eramba/app/upgrade/vendor/eramba/docker || exit
 
 su -s /bin/bash -c "printenv | grep DB_ > .env" www-data
 su -s /bin/bash -c "printenv | grep CACHE_URL >> .env" www-data
+su -s /bin/bash -c "printenv | grep PUBLIC_ADDRESS >> .env" www-data
 su -s /bin/bash -c "printenv | grep HTTP_HOST >> .env" www-data
 
 su -s /bin/bash -c "crontab -u www-data /etc/cron.d/eramba-crontab" www-data
