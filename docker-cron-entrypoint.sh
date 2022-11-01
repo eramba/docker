dc2158e4a185fda6025e910d718733af4420b595
Martin <martin@eramba.org>
2022-11-01 10:45:52 +0100

    Fixed Proxy configuration

--- docker-cron-entrypoint.sh
+++ docker-cron-entrypoint.sh
@@ -6,6 +6,8 @@ su -s /bin/bash -c "printenv | grep DB_ > .env" www-data
 su -s /bin/bash -c "printenv | grep CACHE_URL >> .env" www-data
 su -s /bin/bash -c "printenv | grep PUBLIC_ADDRESS >> .env" www-data
 su -s /bin/bash -c "printenv | grep HTTP_HOST >> .env" www-data
+su -s /bin/bash -c "printenv | grep USE_PROXY >> .env" www-data
+su -s /bin/bash -c "printenv | grep PROXY_ >> .env" www-data
 
 su -s /bin/bash -c "crontab -u www-data /etc/cron.d/eramba-crontab" www-data
 
