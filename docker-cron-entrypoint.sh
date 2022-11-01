37bfb7448966ec0e62289edb17933fa33f6fa462
Martin <martin@eramba.org>
2022-11-01 19:38:49 +0100

    Moved .env file

--- docker-cron-entrypoint.sh
+++ docker-cron-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-su -s /bin/bash -c "printenv | grep DB_ > /tmp/eramba.env" www-data
-su -s /bin/bash -c "printenv | grep CACHE_URL >> /tmp/eramba.env" www-data
-su -s /bin/bash -c "printenv | grep PUBLIC_ADDRESS >> /tmp/eramba.env" www-data
-su -s /bin/bash -c "printenv | grep HTTP_HOST >> /tmp/eramba.env" www-data
-su -s /bin/bash -c "printenv | grep USE_PROXY >> /tmp/eramba.env" www-data
-su -s /bin/bash -c "printenv | grep PROXY_ >> /tmp/eramba.env" www-data
+su -s /bin/bash -c "printenv | grep DB_ > /var/www/eramba/.env" www-data
+su -s /bin/bash -c "printenv | grep CACHE_URL >> /var/www/eramba/.env" www-data
+su -s /bin/bash -c "printenv | grep PUBLIC_ADDRESS >> /var/www/eramba/.env" www-data
+su -s /bin/bash -c "printenv | grep HTTP_HOST >> /var/www/eramba/.env" www-data
+su -s /bin/bash -c "printenv | grep USE_PROXY >> /var/www/eramba/.env" www-data
+su -s /bin/bash -c "printenv | grep PROXY_ >> /var/www/eramba/.env" www-data
 
 su -s /bin/bash -c "crontab -u www-data /etc/cron.d/eramba-crontab" www-data
 
