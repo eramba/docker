3c2337de071fb5740bcee1ccf6dba0736e21d796
Martin <martin@eramba.org>
2022-12-31 14:57:52 +0100

    cron entrypoint runs post-install-cmd

--- docker-cron-entrypoint.sh
+++ docker-cron-entrypoint.sh
@@ -4,6 +4,9 @@ env >> /etc/environment
 
 crontab -u root /etc/cron.d/eramba-crontab
 
+# Run Post Install CMD to generate app_local.php file with unique SALT and other defaults.
+su -s /bin/bash -c "php /var/www/eramba/composer.phar run-script post-install-cmd --working-dir=/var/www/eramba --no-interaction" www-data
+
 su -s /bin/bash -c "php /var/www/eramba/app/upgrade/bin/cake.php queue worker end all" www-data
 
 # execute CMD
